4615fb4683e74d771db0c5e4ba1255dbba991149
LAPTOP-TED0M108\admin <hoainamnguyen2001@gmail.com>
2024-06-26 18:10:51 +0700

    push image

--- src/main/java/com/example/imageservice/controller/UploadController.java
+++ src/main/java/com/example/imageservice/controller/UploadController.java
@@ -2,10 +2,7 @@ package com.example.imageservice.controller;
 
 import com.example.imageservice.service.MinIOService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -26,4 +23,5 @@ public class UploadController {
             return "Failed to upload file " + fileName + ": " + e.getMessage();
         }
     }
+
 }
--- src/main/java/com/example/imageservice/service/MinIOService.java
+++ src/main/java/com/example/imageservice/service/MinIOService.java
@@ -11,8 +11,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
@@ -26,19 +29,28 @@ public class MinIOService {
     @Value("${minio.bucket-name}")
     private String bucketName;
 
+    @Value("${minio.url}")
+    private String minioUrl;
+
+    @Value("${minio.access-key}")
+    private String accessKey;
+
+    @Value("${minio.secret-key}")
+    private String secretKey;
+
     public String uploadFile(MultipartFile file) {
         try {
             // Kiểm tra nếu bucket không tồn tại thì tạo mới
-//            boolean isExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
-//            if (!isExist) {
-//                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
-//            }
+            boolean isExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            if (!isExist) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            }
 
             // Upload file lên MinIO
-            minioClient.putObject(PutObjectArgs.builder().bucket(bucketName).object(file.getName()).stream(file.getInputStream(), -1, 10485760).contentType("image/jpeg").build());
+            minioClient.putObject(PutObjectArgs.builder().bucket(bucketName).object(file.getOriginalFilename()).stream(file.getInputStream(),file.getInputStream().available(), -1 ).contentType("image/jpeg").build());
 
 
-            return "File uploaded successfully: " + file.getName();
+            return "File uploaded successfully: " + file.getOriginalFilename();
         } catch (MinioException | IOException | NoSuchAlgorithmException | InvalidKeyException e) {
             System.err.println("Error occurred: " + e);
             return e.getMessage();
